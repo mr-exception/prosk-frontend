e36f6210ac0cb631722391ce3192b09c3dafb825
mr-exception <darbandi1996@gmail.com>
2018-06-13 13:56:41 +0430

    filters completed

--- src/Containers/Filter.js
+++ src/Containers/Filter.js
@@ -306,16 +306,8 @@ class Filter extends React.Component {
                         <MenuItem value="">
                         </MenuItem>
                         <MenuItem value={-1}>all</MenuItem>
-                        <MenuItem value={0}>0</MenuItem>
-                        <MenuItem value={1}>1</MenuItem>
-                        <MenuItem value={2}>2</MenuItem>
-                        <MenuItem value={3}>3</MenuItem>
-                        <MenuItem value={4}>4</MenuItem>
-                        <MenuItem value={5}>5</MenuItem>
-                        <MenuItem value={6}>6</MenuItem>
-                        <MenuItem value={7}>7</MenuItem>
-                        <MenuItem value={8}>8</MenuItem>
-                        <MenuItem value={9}>9</MenuItem>
+                        <MenuItem value={1}>Doing</MenuItem>
+                        <MenuItem value={3}>Done</MenuItem>
                     </Select>
                 </div>
             </DialogContent>
